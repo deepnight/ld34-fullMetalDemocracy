a56ca66adb0acbac2cee9beb7f793d809a2b9df5
Sébastien Bénard <git@deepnight.net>
2019-01-29 14:14:34 +0100

    Updated minimap

--- src/MiniMap.hx
+++ src/MiniMap.hx
@@ -1,8 +1,10 @@
 import mt.MLib;
+import mt.deepnight.Color;
 
 class MiniMap extends mt.Process {
 	public static var ME : MiniMap;
 
+	var tg : h2d.TileGroup;
 	var bd					: hxd.BitmapData;
 	var bdBase				: hxd.BitmapData;
 	var tex					: h3d.mat.Texture;
@@ -25,17 +27,18 @@ class MiniMap extends mt.Process {
 		var bg = new h2d.Bitmap( h2d.Tile.fromColor(addAlpha(0x0), w+2,h+2), root );
 		bg.setPosition(-1,-1);
 
+		tg = new h2d.TileGroup(Assets.lib.tile, root);
 		bd = new hxd.BitmapData( w,h );
 		tex = h3d.mat.Texture.fromBitmap(bd);
 
 		bdBase = bd.clone();
 		bdBase.fill(0,0,bd.width,bd.height, addAlpha(0x47591C));
 
-		for(pt in level.getSpots("road"))
-			dotCase(bdBase, pt.cx, pt.cy, 0x7E705A);
+		// for(pt in level.getSpots("road"))
+		// 	dotCase(pt.cx, pt.cy, 0x7E705A);
 
-		for(pt in level.getSpots("tree"))
-			dotCase(bdBase, pt.cx, pt.cy, 0x5B7324);
+		// for(pt in level.getSpots("tree"))
+		// 	dotCase(pt.cx, pt.cy, 0x5B7324);
 
 
 		var tf = new h2d.Text(Assets.fontOutline,root);
@@ -61,42 +64,68 @@ class MiniMap extends mt.Process {
 			ME = null;
 	}
 
-	inline function dot(?b:hxd.BitmapData, x:Float, y:Float, c:UInt) {
-		(b==null?bd:b).setPixel( Std.int(x*scale), Std.int(y*scale), addAlpha(c) );
+	inline function dot(x:Float, y:Float, c:UInt) {
+		// (b==null?bd:b).setPixel( Std.int(x*scale), Std.int(y*scale), addAlpha(c) );
+		tg.addColor(
+			Std.int(x*scale), Std.int(y*scale),
+			Color.getR(c), Color.getG(c), Color.getB(c), 1.0,
+			Assets.lib.getTile("pixel")
+		);
 	}
 
-	inline function dotCase(?b:hxd.BitmapData, cx:Int, cy:Int, c:UInt) {
-		(b==null?bd:b).setPixel( Std.int(cx*Const.GRID*scale), Std.int(cy*Const.GRID*scale), addAlpha(c) );
+	inline function dotCase(cx:Int, cy:Int, col:UInt) {
+		// (b==null?bd:b).setPixel( Std.int(cx*Const.GRID*scale), Std.int(cy*Const.GRID*scale), addAlpha(c) );
+		tg.addColor(
+			Std.int(cx*Const.GRID*scale), Std.int(cy*Const.GRID*scale),
+			Color.getR(col), Color.getG(col), Color.getB(col), 1.0,
+			Assets.lib.getTile("pixel")
+		);
 	}
 
-	override public function update() { // TODO unsupported on HL
+	inline function icon(x:Float, y:Float, col:UInt, t:h2d.Tile) {
+		tg.addColor(
+			Std.int(x*scale), Std.int(y*scale),
+			Color.getR(col), Color.getG(col), Color.getB(col), 1.0,
+			t
+		);
+	}
+
+	var blink = false;
+	override public function update() {
 		super.update();
 
-		// if( !cd.hasSetF("refresh",10) ) {
-		// 	var ccount = 0;
+		if( !cd.hasSetF("refresh",10) ) {
+			blink = !blink;
+			var ccount = 0;
+			tg.clear();
 		// 	bd.draw(0,0, bdBase, 0,0, bd.width, bd.height);
 
-		// 	for(cx in 0...level.wid)
-		// 		for(cy in 0...level.hei)
-		// 			if( level.hasCreep(cx,cy) ) {
-		// 				ccount++;
-		// 				dotCase(cx,cy, 0x740E29);
-		// 			}
+			for(pt in level.getSpots("road"))
+				dotCase(pt.cx, pt.cy, 0x7E705A);
 
-		// 	for(e in en.Mob.ALL)
-		// 		if( Std.is(e,en.m.Zergling) )
-		// 			dot(e.x, e.y, 0x8E7BEA );
+			for(pt in level.getSpots("tree"))
+				dotCase(pt.cx, pt.cy, 0x5B7324);
 
-		// 	for(e in en.Mob.ALL)
-		// 		if( Std.is(e,en.m.Creeper) )
-		// 			dot(e.x, e.y, 0xFF0000 );
+			for(cx in 0...level.wid)
+				for(cy in 0...level.hei)
+					if( level.hasCreep(cx,cy) ) {
+						ccount++;
+						dotCase(cx,cy, 0x740E29);
+					}
 
-		// 	for(e in en.Quad.ALL)
-		// 		dot(e.x, e.y, 0xFFFFFF);
+			for(e in en.Mob.ALL)
+				if( Std.is(e,en.m.Zergling) )
+					dot(e.x, e.y, 0x8E7BEA );
 
-		// 	tex.uploadBitmap(bd);
+			for(e in en.Mob.ALL)
+				if( Std.is(e,en.m.Creeper) )
+					icon(e.x, e.y, 0xff0000, Assets.lib.getTile("triangle"));
+					// dot(e.x, e.y, 0xFF0000 );
 
-		// 	creepRatio = ccount/(level.wid*level.hei);
-		// }
+			for(e in en.Quad.ALL)
+				icon(e.x, e.y, blink && Game.ME.current==e ? 0xffffff : e.id==0?0xe1bf15:0xd327d2, Assets.lib.getTile("heart"));
+
+			creepRatio = ccount/(level.wid*level.hei);
+		}
 	}
 }
